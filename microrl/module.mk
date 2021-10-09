3b111eff64ddb43ba4480f26216b1a51330c4d24
Martin Ribelotta <martinribelotta@gmail.com>
2021-10-09 20:45:46 -0300

    First cmake port

--- microrl/module.mk
+++
@@ -1,5 +0,0 @@
-MICRORL_BASE := $(notdir $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST))))))
-
-C_SOURCES += $(MICRORL_BASE)/microrl.c
-
-C_INCLUDES += -I$(MICRORL_BASE)
--- mpaland-printf/module.mk
+++
@@ -1,5 +0,0 @@
-PRINTF_BASE := $(notdir $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST))))))
-
-C_SOURCES += $(PRINTF_BASE)/printf.c
-
-C_INCLUDES += -I$(PRINTF_BASE)
